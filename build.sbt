ee5616f7a93aee15a18caa386cb33962f9d25913
Jichao Ouyang <oyanglulu@gmail.com>
2020-08-30 11:21:19 +1000

    move bootstrap task to plugin

--- build.sbt
+++ build.sbt
@@ -53,16 +53,3 @@ lazy val db = project
     flywayPassword := ""
   )
   .enablePlugins(FlywayPlugin)
-val bootstrap = taskKey[Unit]("couriser boostrap standalone")
-
-bootstrap := {
-  publishLocal.value;
-  import sys.process._
-  val logger = sbt.Keys.streams.value.log
-  val res =
-    s"coursier bootstrap --standalone ${organization.value}::${name.value}:${version.value} -f -o ${name.value}" !;
-  if (res == 0)
-    logger.info(s"to run simply enter `./${name.value}`")
-  else
-    sys.error("boostrap failed!")
-}
---
+++ project/BootstrapPlugin.scala
@@ -0,0 +1,20 @@
+import sbt.Keys._
+import sbt._
+
+object BootstrapPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+  val bootstrap = taskKey[Unit]("couriser boostrap standalone")
+  override lazy val projectSettings = Seq(
+    bootstrap := {
+      publishLocal.value;
+      import sys.process._
+      val logger = sbt.Keys.streams.value.log
+      val res =
+        s"coursier bootstrap --standalone ${organization.value}::${name.value}:${version.value} -f -o ${name.value}" !;
+      if (res == 0)
+        logger.info(s"to run simply enter `./${name.value}`")
+      else
+        sys.error("boostrap failed!")
+    }
+  )
+}
